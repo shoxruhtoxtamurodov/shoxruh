30745bdc4c609b14a4d120aad5d9456e6214b1ea
shoxruh <shoxruhtoxtamurodov40@gmail.com>
2023-01-29 16:16:43 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello world! 
     </h1>
+    
     </div>
   );
 }
 
-export default App
+export default App;
