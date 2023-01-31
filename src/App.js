ff7da619c806273e4555208355c268a996156edc
shoxruh <shoxruhtoxtamurodov40@gmail.com>
2023-01-31 12:32:35 +0500

    banner-created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import './App.css';
-import Main from './Main';
+import "./App.css";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world! 
-    </h1>
-    <h3>salom hammaga</h3>
-    <Main/>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h3>salom hammaga</h3>
+      <Main />
     </div>
   );
 }
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Banner from './Banner';
 
 function Main() {
   return (
     <div>
          <h1>salom hammaga </h1>
+         <button>add</button>
+         <Banner/>
     </div>
   )
 }
 
-export default Main
+export default Main;       
