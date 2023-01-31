45dd7f37d06d35ea4912b5965229535ab1e06bb3
shoxruh <shoxruhtoxtamurodov40@gmail.com>
2023-01-31 12:05:51 +0500

    main component ozgardi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Main from './Main';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       Hello world! 
     </h1>
     <h3>salom hammaga</h3>
-    
+    <Main/>
     </div>
   );
 }
--- src/Main.jsx
+++ src/Main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Main() {
   return (
     <div>
-         
+         <h1>salom hammaga </h1>
     </div>
   )
 }
